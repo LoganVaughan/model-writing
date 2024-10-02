9a4c41633d37852b25e6d4f415b1ff1d962e4418
Logan Vaughan <LBVaughan25@gmail.com>
2024-10-02 10:21:18 -0400

    asd

--- index.js
+++ index.js
@@ -3,15 +3,27 @@
 CSS Business Model
 
 Sectors of Business:
-Operations
-Marketing 
-Finance 
-Customer Service 
+-Operations
+-Marketing 
+-Finance 
+-Customer Service 
 
 Properties:
+-IsSeasonal
+-LoganJob 
+-EthanJob
+-AllyJob 
+-TimeCommitment 
+-
 
 Events:
+-Hour Worked 
+-Social Media Post
+-Weekly Meeting
 
 Methods:
+-Money Made
+-Increase in website traffic 
+-New homework for founders 
 
 */
